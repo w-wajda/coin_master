a00181d081be9987595ad2467a67feb535a9b384
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-12 23:38:47 +0100

    feat: update command to create token

--- app/application/commands/tokens/create_token.py
+++ app/application/commands/tokens/create_token.py
@@ -4,10 +4,7 @@ from app.domain.tokens.token import Token
 from app.domain.tokens.token_repository import ITokenRepository
 from app.domain.users.user_exceptions import InvalidUserCredentials
 from app.domain.users.user_repository import IUserRepository
-from app.domain.users.user_schemas import (
-    UserLoginSchema,
-    UserRequestMeta,
-)
+from app.domain.users.user_schemas import UserLoginSchema
 
 
 logger = logging.getLogger(__name__)
@@ -18,7 +15,7 @@ class CreateTokenCommand:
         self.user_repository = user_repository
         self.token_repository = token_repository
 
-    async def __call__(self, user_login_data: UserLoginSchema, user_request_meta: UserRequestMeta) -> Token:
+    async def __call__(self, user_login_data: UserLoginSchema) -> Token:
         logger.info(f"Creating token for: {user_login_data.email=}")
         async with self.user_repository.start_session() as session:
             self.token_repository.use_session(session)
@@ -28,7 +25,7 @@ class CreateTokenCommand:
             if not user or not user.check_password(user_login_data.password):
                 raise InvalidUserCredentials(email=user_login_data.email)
 
-            token = Token(user=user, **user_request_meta.model_dump())
+            token = Token(user=user)
             self.token_repository.add(token)
             await self.token_repository.commit()
 
