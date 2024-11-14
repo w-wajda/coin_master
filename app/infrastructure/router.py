b2485c410c9673ba2b4501768bee5d8cfa2296a1
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-14 23:54:32 +0100

    feat: add router for endpoints

--- app/infrastructure/router.py
+++ app/infrastructure/router.py
@@ -3,7 +3,12 @@ from fastapi import (
     Depends,
 )
 from fastapi.security import HTTPBearer
+from app.presentation.endpoints.companies import routes as companies_routes
+from app.presentation.endpoints.email_templates import routes as email_templates_routes
+from app.presentation.endpoints.receipts import routes as receipts_routes
+from app.presentation.endpoints.tokens import routes as tokens_routes
 from app.presentation.endpoints.users import routes as users_routes
+
 
 router = APIRouter()
 security = HTTPBearer(auto_error=False)
@@ -25,7 +30,8 @@ async def trigger_error():
 async def version():
     return {"version": "0.1.0"}
 
-
-router.include_router(users_routes, prefix="/v2/users", dependencies=[Depends(security)])
+router.include_router(companies_routes, prefix="/v2/companies", dependencies=[Depends(security)])
+router.include_router(email_templates_routes, prefix="/v2/email_templates", dependencies=[Depends(security)])
+router.include_router(receipts_routes, prefix="/v2/receipts", dependencies=[Depends(security)])
 router.include_router(tokens_routes, prefix="/v2/tokens", dependencies=[Depends(security)])
-
+router.include_router(users_routes, prefix="/v2/users", dependencies=[Depends(security)])
