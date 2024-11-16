af0eaabdfb6291f00b610dd7cd09a993d63501db
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-16 21:29:07 +0100

    feat: update hosts

--- scripts/install_hosts.sh
+++ scripts/install_hosts.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # This script will install the hosts file to /etc/hosts
-GLM_ENTRY="127.0.0.1 cm.local api.cm.local img.cm.local minio.cm.local mailhog.cm.local"
+CM_ENTRY="127.0.0.1 cm.local api.cm.local img.cm.local minio.cm.local mailhog.cm.local"
 
 add_if_host_not_exists () {
-    if grep -q "$GLM_ENTRY" /etc/hosts; then
+    if grep -q "$CM_ENTRY" /etc/hosts; then
         echo "Hosts file already contains CM entry"
     else
         echo "Adding CM entry to hosts file"
-        sudo -- sh -c -e "echo '$GLM_ENTRY' >> /etc/hosts";
+        sudo -- sh -c -e "echo '$CM_ENTRY' >> /etc/hosts";
     fi
 }
 
