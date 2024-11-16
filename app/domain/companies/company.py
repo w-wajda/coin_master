441374f6b66050bca506ac950d29dc537f67c987
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-16 21:48:38 +0100

    feat: add new table Company in database

---
+++ app/domain/companies/company.py
@@ -0,0 +1,11 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.domain.common.base import Base
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    name: Mapped[str] = mapped_column(sa.String(100), nullable=False)
+    address: Mapped[str] = mapped_column(sa.String(255), nullable=True)
