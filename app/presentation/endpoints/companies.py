0136423375e68422e137af317871afca80017387
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-17 20:42:44 +0100

    feat: add endpoint get_company

--- app/presentation/endpoints/companies.py
+++ app/presentation/endpoints/companies.py
@@ -1,4 +1,21 @@
-from fastapi import APIRouter
+from uuid import UUID
 
+from dependency_injector.wiring import inject, Provide
+from fastapi import APIRouter, Depends
+
+from app.application.queries.companies.get_company import GetCompanyQuery
+from app.domain.companies.company_schemas import CompanySchema
+from app.infrastructure.decorators import requires_auth
+from app.infrastructure.di import AppContainer
 
 routes = APIRouter()
+
+@routes.get("/get/{uuid}/", tags=["Authenticated"])
+@requires_auth()
+@inject
+async def get_company(
+    uuid: UUID,
+    get_company_query: GetCompanyQuery = Depends(Provide[AppContainer.queries.get_company]),
+) -> CompanySchema:
+    company = await get_company_query(uuid)
+    return CompanySchema.model_validate(company)
