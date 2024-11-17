e9bfca0f17ae299448a14e932ab2f2ca8a97be7f
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-17 22:07:47 +0100

    feat: add endpoint update_company

---
+++ app/application/commands/companies/update_company.py
@@ -0,0 +1,23 @@
+from uuid import UUID
+
+from starlette import status
+
+from app.domain.companies.company import Company
+from app.domain.companies.company_repository import ICompanyRepository
+from app.domain.companies.company_schemas import UpdateCompanySchema
+from app.domain.exceptions import HTTPException
+
+
+class UpdateCompanyCommand:
+    def __init__(self, company_repository: ICompanyRepository):
+        self.company_repository = company_repository
+
+    async def __call__(self, uuid: UUID, company_data: UpdateCompanySchema) -> Company:
+        async with self.company_repository.start_session():
+
+            if company := await self.company_repository.get_by(uuid=uuid):
+                company.update(**company_data.model_dump(exclude_unset=True))
+                await self.company_repository.commit()
+                return company
+
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
--- app/presentation/endpoints/companies.py
+++ app/presentation/endpoints/companies.py
@@ -11,6 +11,7 @@ from fastapi import (
 from starlette import status
 
 from app.application.commands.companies.create_company import CreateCompanyCommand
+from app.application.commands.companies.update_company import UpdateCompanyCommand
 from app.application.queries.companies.get_company import GetCompanyQuery
 from app.application.queries.companies.get_company_list import GetCompanyListQuery
 from app.application.services.pagination import (
@@ -20,6 +21,7 @@ from app.application.services.pagination import (
 from app.domain.companies.company_schemas import (
     CompanySchema,
     CreateCompanySchema,
+    UpdateCompanySchema,
 )
 from app.infrastructure.decorators import requires_auth
 from app.infrastructure.di import AppContainer
@@ -50,7 +52,7 @@ async def get_company_list(
     return pagination.get_items(companies)
 
 
-@routes.get("/create/", tags=["Authenticated"], status_code=status.HTTP_201_CREATED)
+@routes.post("/create/", tags=["Authenticated"], status_code=status.HTTP_201_CREATED)
 @requires_auth()
 @inject
 async def create_company(
@@ -58,4 +60,16 @@ async def create_company(
     create_company_command: CreateCompanyCommand = Depends(Provide[AppContainer.commands.create_company]),
 ) -> CompanySchema:
     company = await create_company_command(company_data)
+    return CompanySchema.model_validate(company)
+
+
+@routes.patch("/update/{uuid}", tags=["Authenticated"], status_code=status.HTTP_200_OK)
+@requires_auth()
+@inject
+async def update_company(
+    uuid: UUID,
+    company_data: UpdateCompanySchema,
+    update_company_command: UpdateCompanyCommand = Depends(Provide[AppContainer.commands.update_company]),
+) -> CompanySchema:
+    company = await update_company_command(uuid, company_data)
     return CompanySchema.model_validate(company)
