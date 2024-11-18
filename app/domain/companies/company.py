0e221ac0635d2832dd60cebaeee792a439f6f7b8
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-18 23:52:12 +0100

    feat: add new field: user

--- app/domain/companies/company.py
+++ app/domain/companies/company.py
@@ -2,9 +2,11 @@ import sqlalchemy as sa
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
+    relationship,
 )
 
 from app.domain.common.base import Base
+from app.domain.users.user import User
 
 
 class Company(Base):
@@ -12,3 +14,6 @@ class Company(Base):
 
     name: Mapped[str] = mapped_column(sa.String(100), nullable=False)
     address: Mapped[str] = mapped_column(sa.String(255), nullable=True)
+
+    user_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("users.id"))
+    user: Mapped[User] = relationship(backref="companies", foreign_keys="Company.user_id")
