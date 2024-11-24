1daec35f9c9457511b0052b66468ee9a642c6a8e
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2024-11-24 18:11:45 +0100

    feat: add GetTagQuery

---
+++ app/application/queries/tags/get_tag.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+
+from starlette import status
+
+from app.domain.exceptions import HTTPException
+from app.domain.tags.tag import Tag
+from app.domain.tags.tag_repository import ITagRepository
+from app.domain.users.user_repository import IUserRepository
+
+
+class GetTagQuery:
+    def __init__(self, user_repository: IUserRepository, tag_repository: ITagRepository):
+        self.user_repository = user_repository
+        self.tag_repository = tag_repository
+
+    async def __call__(self, user_id: int, uuid: UUID) -> Tag:
+        async with self.user_repository.start_session() as session:
+            self.tag_repository.use_session(session)
+
+        user = await self.user_repository.get(user_id)
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+        if tag := await self.tag_repository.get_by(user=user, uuid=uuid):
+            return tag
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag noy found")
+
+
